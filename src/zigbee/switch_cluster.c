f1e99c12da9057f4527ece5cfcacc6531bc61cd1
marazmarci <marazmarci@gmail.com>
2025-10-06 11:24:15 +0200

    Increase presses required to reset device to 30

--- src/zigbee/switch_cluster.c
+++ src/zigbee/switch_cluster.c
@@ -12,7 +12,7 @@
 #include "relay_cluster.h"
 #include "zigbee_commands.h"
 
-#define MULTI_PRESS_CNT_TO_RESET 10
+#define MULTI_PRESS_CNT_TO_RESET 30
 
 const uint8_t multistate_out_of_service = 0;
 const uint8_t multistate_flags = 0;
