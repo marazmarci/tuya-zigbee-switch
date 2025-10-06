09377cd8799ce51821244764ef809cd5757b4576
marazmarci <marazmarci@gmail.com>
2025-10-06 10:55:45 +0200

    fix: Install yq directly, because it isn't available from apt

--- make_scripts/make_install.sh
+++ make_scripts/make_install.sh
@@ -7,7 +7,10 @@ set -e                                           # Exit on error.
 cd "$(dirname "$(dirname "$(realpath "$0")")")"  # Go to project root.
 
 sudo apt-get update
-sudo apt-get install -y make python3 python3-jinja2 python3-yaml yq unzip wget
+sudo apt-get install -y make python3 python3-jinja2 python3-yaml unzip wget
+
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/local/bin/yq && \
+    sudo chmod +x /usr/local/bin/yq
 
 make clean_install
 make install
